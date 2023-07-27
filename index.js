97624c6e9bf97ee4ce669802693233caa5d4690b
Divyani Hazari <dhazari210@gmail.com>
2023-07-27 14:38:29 -0500

    refactored the blackjack game

--- index.js
+++ index.js
@@ -5,26 +5,46 @@ let titleEl = document.getElementById("title-el");
 let num1 =  getRandomCardFromDeck();
 let num2 =  getRandomCardFromDeck();
 let sum = num1 + num2;
-let textSum = num1 + " " + num2;
+let displayNumbers = num1 + " " + num2;
+
+let isGameOver = false;
+
 
 function startGame() {
-    cardEl.textContent = "Cards: " + textSum;    
+    
+    //if isgameover is true then 
+    // 1. set to false
+    // 2. set random numbers and sum
+    if(isGameOver === true) {
+        isGameOver = false;
+        num1 = getRandomCardFromDeck();
+        num2 = getRandomCardFromDeck();
+        sum = num1 + num2;
+        displayNumbers = num1 + " " + num2;
+
+    }
+    cardEl.textContent = "Cards: " + displayNumbers;    
     sumEl.textContent = "Sum: " + sum;
     if(sum < 21){
         titleEl.textContent = "You have chance to take a card!";
+        isGameOver = false;
     } else if (sum === 21) {
         titleEl.textContent = "You are winner!";
+        isGameOver = true;
     } else {
         titleEl.textContent = "You are lost";
+        isGameOver = true;
     }
 }
 
 function newCard() {
-    let num = getRandomCardFromDeck();
-    console.log(num);
-    textSum = textSum + " " + num;
-    sum += num;
+    if(isGameOver === false) {
+        let num = getRandomCardFromDeck();
+        displayNumbers = displayNumbers + " " + num;
+        sum += num; 
     startGame();
+
+    }
 }
 
 function getRandomCardFromDeck() {
